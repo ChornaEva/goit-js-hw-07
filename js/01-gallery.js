2a844a637a4657ef0e74ad44e15078224f28b02a
ChornaEva <chornaja.evgenija@gmail.com>
2021-10-16 20:40:04 +0300

    first task done

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,35 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const gallaryNode = document.querySelector('.gallery');
+
+const newGallery = galleryItems.map(element =>
+  `<div class="gallery__item">
+  <a class="gallery__link" href="${element.original}">
+    <img
+      class="gallery__image"
+      src="${element.preview}"
+      data-source="${element.original}"
+      alt="${element.description}"
+    />
+  </a>
+</div>`).join('');
+
+gallaryNode.insertAdjacentHTML('beforeend', newGallery);
+
+const openLargeImage =  (event) => {
+  event.preventDefault();
+
+  const largeImage = event.target.dataset.source;
+  const modalWindow = basicLightbox.create(`<img width="1400" height="900" src="${largeImage}">`);
+  modalWindow.show();
+
+  document.addEventListener("keydown", event => {
+    if (event.key === 'Escape') {
+      modalWindow.close();
+      document.removeEventListener("keydown", openLargeImage);
+    }
+  })
+}
+
+gallaryNode.addEventListener('click', openLargeImage);
